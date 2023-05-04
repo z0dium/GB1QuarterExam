92d271da958cf35809ed4d733788cfefedaba6c9
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-05-04 23:39:56 +0300

    created repository, added input section for program

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Заполните массив строк для обработки. Для завершения наполнения массива введите пустую строку.");
+string input = Console.ReadLine();
+string initialArray = "";
+while (input.Length > 0) {
+    initialArray = initialArray + input + "\n";
+    input = Console.ReadLine();
+}
+
